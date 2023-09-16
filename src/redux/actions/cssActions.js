e35a50a901d0da07de53d83af44332539d1e0372
Horvi <ivana.hrvtc@gmail.com>
2023-09-16 19:13:12 +0200

    Delete src directory

--- src/redux/actions/cssActions.js
+++
@@ -1,8 +0,0 @@
-import { UPDATE_MARGIN_LEFT } from '../reducers/cssReducer'
-
-export const updateMarginLeft = (marginLeft) => {
-    return {
-        type: UPDATE_MARGIN_LEFT,
-        payload: marginLeft,
-    }
-}
--- src/redux/reducers/index.js
+++
@@ -1,8 +0,0 @@
-import { combineReducers } from "redux";
-import cssReducer from "./cssReducer";
-
-const rootReducer = combineReducers({
-    css: cssReducer
-})
-
-export default rootReducer
